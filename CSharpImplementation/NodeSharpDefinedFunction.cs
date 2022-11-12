62bbd79a14a6bceb939a99fda4aeaf46c336d504
joshf67 <iownall99@live.co.uk>
2022-11-12 08:18:37 +0000

    Create NodeSharpDefinedFunction.cs
    Holds any data that is needed for events/function calls

---
+++ CSharpImplementation/NodeSharpDefinedFunction.cs
@@ -0,0 +1,28 @@
+﻿using Antlr4.Runtime.Tree;
+using NodeSharp.Grammar;
+using NodeSharp.Nodes.Interface;
+
+namespace NodeSharp;
+
+public class NodeSharpDefinedFunction
+{
+    public List<ITerminalNode> FunctionParameters = new List<ITerminalNode>();
+    public NodeSharpParser.BlockContext? FunctionBlock;
+    public IActionNode? EventNode = null;
+    public string FunctionName;
+    
+    public NodeSharpDefinedFunction(string funcName, NodeSharpParser.FunctionCreationParametersContext[]? parameters = null, NodeSharpParser.BlockContext? funcBlock = null)
+    {
+        if (parameters != null && parameters?.Length == 1)
+        {
+            FunctionParameters = parameters[0].IDENTIFIER().ToList();
+        }
+        else
+        {
+            FunctionParameters = new List<ITerminalNode>();
+        }
+
+        FunctionBlock = funcBlock;
+        FunctionName = funcName;
+    }
+}
