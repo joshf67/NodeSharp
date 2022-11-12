c0068d34e359993220af32a7eca4090349194479
joshf67 <iownall99@live.co.uk>
2022-11-12 08:21:04 +0000

    Added node connection classes
    Add classes to hold data about the flow between action nodes

---
+++ CSharpImplementation/Action Scope/ActionScope.cs
@@ -0,0 +1,41 @@
+﻿using NodeSharp.Nodes.Interface;
+
+namespace NodeSharp;
+
+public class ActionScope
+{
+    public ActionScope? ParentScope;
+    public string? ParentPin;
+    
+    public IActionNode ActionNode;
+    
+    private List<ActionScope> ScopeNodes = new List<ActionScope>();
+    private int CurrentScope = 0;
+    
+    public ActionScope(IActionNode actionNode, ActionScope? parentScope = null, string? parentPin = null)
+    {
+        ParentScope = parentScope;
+        ParentPin = parentPin;
+        ActionNode = actionNode;
+    }
+
+    public ActionScope AddScope(IActionNode actionNode, string parentPin)
+    {
+        var newScope = new ActionScope(actionNode, this, parentPin);
+        ScopeNodes.Add(newScope);
+        return newScope;
+    }
+
+    public ActionScope? GetNextNode()
+    {
+        while (true)
+        {
+            if (CurrentScope >= ScopeNodes.Count) return null;
+            var ret = ScopeNodes[CurrentScope].GetNextNode();
+
+            if (ret != null) return ret;
+            return ScopeNodes[CurrentScope++];
+            //throw new InvalidOperationException($"Trying to return next Node, out of bounds");
+        }
+    }
+}
---
+++ CSharpImplementation/Action Scope/ActionScopeShell.cs
@@ -0,0 +1,28 @@
+﻿namespace NodeSharp;
+
+using NodeSharp.Nodes.Interface;
+
+/// <summary>
+/// Allows branching by faking node to be replaced later with actual node
+/// </summary>
+public class ActionNodeShell : IActionNode
+{
+    public string ReceivePin;
+    public string SendPin;
+    
+    public ActionNodeShell(string receivePin, string sendPin)
+    {
+        ReceivePin = receivePin;
+        SendPin = sendPin;
+    }
+    
+    public string? GetActionReceivePin()
+    {
+        return ReceivePin;
+    }
+
+    public string? GetActionSendPin()
+    {
+        return SendPin;
+    }
+}
