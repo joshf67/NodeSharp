d6f8e80a54f13f481806d5f05143703c6bb2f203
joshf67 <iownall99@live.co.uk>
2022-11-12 08:21:22 +0000

    Create Identifier.cs
    Moved Identifiers out to their own class to hold extra data required

---
+++ NodeGraph/NodeData/Identifier.cs
@@ -0,0 +1,19 @@
+﻿namespace NodeSharp.NodeGraph.NodeData;
+
+public class Identifier
+{
+    public string IdentifierName = "";
+    public int Order = -1;
+    public int UseCount = 0;
+
+    public Identifier(string name, int order)
+    {
+        IdentifierName = name;
+        Order = order;
+    }
+
+    public void AddUse()
+    {
+        UseCount++;
+    }
+}
