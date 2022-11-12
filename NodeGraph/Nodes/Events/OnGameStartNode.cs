6bfe65da3d0847549aecbf74d39477cadb79e84f
joshf67 <iownall99@live.co.uk>
2022-11-12 08:18:52 +0000

    Create OnGameStartNode.cs
    Added GameStart node for initial start node

---
+++ NodeGraph/Nodes/Events/OnGameStartNode.cs
@@ -0,0 +1,25 @@
+﻿using NodeSharp.NodeGraph.NodeData;
+using NodeSharp.Nodes.Interface;
+
+namespace NodeSharp.Nodes.Events_Custom;
+
+public class OnGameStartNode : Node, IActionNode
+{
+    public OnGameStartNode(ScriptBrain brain) : base(brain, NodeTypes.OnGameStart)
+    {
+        Pins = new[]
+        {
+            new Pin(Keywords.EventTriggered, 0)
+        };
+    }
+
+    public string? GetActionSendPin()
+    {
+        return Keywords.EventTriggered;
+    }
+
+    public string? GetActionReceivePin()
+    {
+        return null;
+    }
+}
