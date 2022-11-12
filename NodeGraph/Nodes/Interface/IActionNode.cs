ea80d3b068624d982ed48dab8f1d63643a29406c
joshf67 <iownall99@live.co.uk>
2022-11-12 07:55:22 +0000

    Create IActionNode.cs
    Allows custom handling of enter/exit pins flow

---
+++ NodeGraph/Nodes/Interface/IActionNode.cs
@@ -0,0 +1,14 @@
+﻿namespace NodeSharp.Nodes.Interface;
+
+public interface IActionNode
+{
+    public string? GetActionReceivePin()
+    {
+        return Keywords.ActionStart;
+    }
+
+    public string? GetActionSendPin()
+    {
+        return Keywords.ActionComplete;
+    }
+}
