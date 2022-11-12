f70a33c3612770a95d80a79b195fd9b94c674382
joshf67 <iownall99@live.co.uk>
2022-11-12 08:17:16 +0000

    Create AntlrErrorListener.cs
    Added custom ANTLR error for any invalid parses/tokens in input

---
+++ CSharpImplementation/AntlrErrorListener.cs
@@ -0,0 +1,15 @@
+﻿using Antlr4.Runtime;
+
+namespace NodeSharp;
+
+public class AntlrErrorListener : IAntlrErrorListener<IToken>
+{
+    public void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg,
+        RecognitionException e)
+    {
+        if (offendingSymbol is CommonToken token)
+            throw new Exception($"Unknown input: '{token.Text}', at {line}:{charPositionInLine}");
+
+        throw new NotImplementedException();
+    }
+}
