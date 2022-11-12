debc4d8f072a21a68c9f5130186e6b7da3a5b488
joshf67 <iownall99@live.co.uk>
2022-11-12 07:54:35 +0000

    Create IConstantType.cs
    Determines if a variable node is constant and converts its type into a non-constant version

---
+++ NodeGraph/Nodes/Interface/IConstantType.cs
@@ -0,0 +1,9 @@
+﻿namespace NodeSharp.Nodes.Interface;
+
+public interface IConstantType
+{
+    public Type GetVariableNodeType();
+    // {
+    //     throw new NotImplementedException("This constant type does not have a variable type declared");
+    // }
+}
