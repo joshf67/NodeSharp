6303c6ab6d31e0a4b9df61ec93126e9260a562d7
joshf67 <iownall99@live.co.uk>
2022-11-12 07:53:44 +0000

    Create ISetterOptimizable.cs
    Determines if a setter can be optimized and handles how it should be optimized

---
+++ NodeGraph/Nodes/Interface/ISetterOptimizable.cs
@@ -0,0 +1,9 @@
+﻿namespace NodeSharp.Nodes.Interface;
+
+public interface ISetterOptimizable
+{
+    void DefaultSetter(object? value)
+    {
+        throw new NotImplementedException("Setter node default has not been implemented on this node type");
+    }
+}
