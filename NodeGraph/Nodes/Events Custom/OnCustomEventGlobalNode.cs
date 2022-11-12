5ef09d50578902f40191c1b5fc60d246e1c725a4
joshf67 <iownall99@live.co.uk>
2022-11-12 08:15:43 +0000

    Added Function/Event nodes

---
+++ NodeGraph/Nodes/Events Custom/OnCustomEventGlobalNode.cs
@@ -0,0 +1,37 @@
+﻿using NodeSharp.NodeGraph.NodeData;
+using NodeSharp.Nodes.Interface;
+
+namespace NodeSharp.Nodes.Events_Custom;
+
+public class OnCustomEventGlobalNode : Node, IActionNode
+{
+    public OnCustomEventGlobalNode(ScriptBrain brain, VariableData data) : base(brain, NodeTypes.OnCustomEventGlobal)
+    {
+        ImplementationNodeData = new[]
+        {
+            new Property(null, new List<Values>()
+            {
+                new Values(Keywords.Identifier, DataType.UShort, data.Identifier)
+            })
+        };
+
+        Pins = new[]
+        {
+            new Pin(Keywords.Identifier, 0),
+            new Pin(Keywords.Number, 0),
+            new Pin(Keywords.EventTriggered, 0),
+            new Pin(Keywords.ObjectList, 0),
+            new Pin(Keywords.Object, 0)
+        };
+    }
+    
+    public string? GetActionSendPin()
+    {
+        return Keywords.EventTriggered;
+    }
+
+    public string? GetActionReceivePin()
+    {
+        return null;
+    }
+}
---
+++ NodeGraph/Nodes/Events Custom/TriggerCustomEventGlobalNode.cs
@@ -0,0 +1,39 @@
+﻿using NodeSharp.NodeGraph.NodeData;
+using NodeSharp.Nodes.Interface;
+
+namespace NodeSharp.Nodes.Events_Custom;
+
+public class TriggerCustomEventGlobalNode : Node, IActionNode
+{
+    public TriggerCustomEventGlobalNode(ScriptBrain brain, VariableData data) : base(brain, NodeTypes.TriggerCustomEventGlobal)
+    {
+        ImplementationNodeData = new[]
+        {
+            new Property(null, new List<Values>()
+            {
+                new Values(Keywords.Number, DataType.Float, Convert.ToSingle(data.Data ?? 0)),
+                new Values(Keywords.Identifier, DataType.UShort, data.Identifier)
+            })
+        };
+
+        Pins = new[]
+        {
+            new Pin(Keywords.ActionStart, 0),
+            new Pin(Keywords.Identifier, 0),
+            new Pin(Keywords.ObjectList, 0),
+            new Pin(Keywords.Object, 0),
+            new Pin(Keywords.Number, 0),
+            new Pin(Keywords.ActionComplete, 0)
+        };
+    }
+
+    public string? GetActionSendPin()
+    {
+        return Keywords.ActionComplete;
+    }
+
+    public string? GetActionReceivePin()
+    {
+        return Keywords.ActionStart;
+    }
+}
